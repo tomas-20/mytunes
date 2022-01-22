4d64803b0d2f3d1e3017571cb1e4b6159c6d2411
tomas-20 <tacuna20@stuy.edu>
2022-01-22 13:22:52 -0500

    added the title_is and artist_is functions to song.h

--- song.h
+++ song.h
@@ -5,3 +5,5 @@ struct song *get_song(char *, char *);
 void free_song(struct song *);
 void print_song(struct song *);
 int songcmp(struct song *, struct song *);
+int title_is(struct song *, char *);
+int artist_is(struct song *, char *);
--- test.c
+++ test.c
@@ -9,11 +9,24 @@ void compare_songs(struct song *a, struct song *b) {
   printf("%d\n", songcmp(a, b));
 }
 
+void check_artist(struct song *cancion, char *artist) {
+  print_song(cancion);
+  if (artist_is(cancion, artist)) {
+    printf(" is by ");
+  }
+  else {
+    printf(" is not by ");
+  }
+  printf("%s\n", artist);
+}
+
 int main() {
   struct song *dinosaurios = get_song("los dinosaurios", "charly garcia");
   struct song *west = get_song("go west", "village people");
   compare_songs(dinosaurios, west);
   compare_songs(west, dinosaurios);
+  check_artist(dinosaurios, "bob dylan");
+  check_artist(dinosaurios, "charly garcia");
   free_song(dinosaurios);
   free_song(west);
   return 0;
