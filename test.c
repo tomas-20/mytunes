757af0ac54a5d59197c2069993324bc7e6e9b73a
tomas-20 <tacuna20@stuy.edu>
2022-01-22 15:10:45 -0500

    added more songs to test.c

--- test.c
+++ test.c
@@ -5,10 +5,17 @@ int main() {
   struct song_list *list = NULL;
   list = add_song(list, "ymca", "village people");
   list = add_song(list, "los dinosaurios", "charly garcia");
+  list = add_song(list, "i'm a princess", "bill wurtz");
   list = add_song(list, "go west", "village people");
+  list = add_song(list, "la de da", "bill wurtz");
+  list = add_song(list, "just did a bad thing", "bill wurtz");
   print_song_list(list);
   printf("\n");
+  printf("songs by village people: ");
   print_songs_by_artist(list, "village people");
+  printf("\n");
+  printf("songs by bill wurtz: ");
+  print_songs_by_artist(list, "bill wurtz");
   printf("\n");
   free_song_list(list);
   return 0;
